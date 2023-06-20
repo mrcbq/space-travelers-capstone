cb49351d10106e148801586442d4f5b2608df94b
unknown <teddytiyi@gmail.com>
2023-06-20 10:02:44 +0200

    modified NavBar and Add NavBar.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,51 +1,21 @@
-import { Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../logo.png';
+import './NavBar.css';
 
 const NavBar = () => (
-  <Navbar bg="light" expand="lg" className="mx-5">
-    <Navbar.Brand href="">
+  <div className="top">
+    <h1 className="title">
       <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />
-      {' '}
       Space Travelers&apos; Hub
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-between">
-      <Nav>
-        <Nav.Link>
-          {' '}
-          <NavLink
-            exact
-            to="/"
-            className="text-decoration-none text-dark"
-            activeClassName="text-decoration-underline"
-          >
-            <span className="mr-5">Rockets</span>
-          </NavLink>
-        </Nav.Link>
-        <Nav.Link>
-          <NavLink
-            exact
-            to="/Mission"
-            className="text-decoration-none text-dark"
-            activeClassName="text-decoration-underline"
-          >
-            Mission
-          </NavLink>
-        </Nav.Link>
-        <Nav.Link>
-          <NavLink
-            exact
-            to="/MyProfile"
-            className="text-decoration-none text-dark"
-            activeClassName="text-decoration-underline"
-          >
-            My Profile
-          </NavLink>
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+    </h1>
+    <nav className="nav">
+      <Link className="a" to="/">Rockets</Link>
+      <hr />
+      <Link className="a" to="/Mission">Mission</Link>
+      <hr />
+      <Link className="a" to="/MyProfile">My Profile</Link>
+    </nav>
+  </div>
 );
 
 export default NavBar;
