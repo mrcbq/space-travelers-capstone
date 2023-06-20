760b679dcb9895a5bfcd9e7a20c6221b95a1d108
mrcbq <marcobaquero@gmail.com>
2023-06-20 17:06:43 -0500

    Add UseEffect fro execute only once getMissions fetch action

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getMissions } from '../redux/missions/missionsSlice';
+import { useEffect } from 'react';
 
 export default function Missions() {
   const missions = useSelector((store) => store.missions);
   const dispatch = useDispatch();
-  dispatch(getMissions());
+
+  useEffect(() => {
+    if(missions.missionsArr.length === 0) {
+      dispatch(getMissions());
+    }
+  }, [])
+
   console.log('Hello');
   console.log(missions);
   return <div>missions</div>;
