cd4507b3b41c9658618a7ac16125386fea40e590
mrcbq <marcobaquero@gmail.com>
2023-06-21 17:39:19 -0500

    Add ul for display the missions reserved

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -2,40 +2,33 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 // import MyProfile from './MyProfile';
 import { getMissions } from '../redux/missions/missionsSlice';
+// import { fetchRockets } from '../redux/Rocket/RocketSlice';
 
 function ProfileList() {
   const { missions } = useSelector((store) => store.missions);
+  // const { rockets } = useSelector((store) => store.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (missions.length === 0) {
       dispatch(getMissions());
     }
-    // if (Rockets.length === 0) {
-    //   dispatch(getMissions());
+    // if (rockets.length === 0) {
+    //   dispatch(fetchRockets());
     // }
   }, []);
-
-  // const rockets = useSelector((store) => store.rockets);
-  // const filteredRockets = rockets.rocketsArr.filter(
-  //   (rocket) => rocket.reserved === true,
-  // );
 
   const filteredMissions = missions.filter(
     (mission) => mission.isMember === true,
   );
-  console.log(filteredMissions);
+  // const filteredRockets = rockets.filter(
+  //   (rocket) => rocket.reserved === true,
+  // );
+
+  // console.log(filteredRockets);
 
   return (
     <section>
-      {/* <div>
-        <h2>My Rockets</h2>
-        <ul>
-          {filteredMissions.map((item) => (
-            <li key={item.mission_id}>{item.mission_name}</li>
-          ))}
-        </ul>
-      </div> */}
       <div>
         <h2>My Missions</h2>
         <ul>
@@ -44,6 +37,14 @@ function ProfileList() {
           ))}
         </ul>
       </div>
+      {/* <div>
+        <h2>My Rockets</h2>
+        <ul>
+          {filteredRockets.map((item) => (
+            <li key={item.rocket_id}>{item.rocket_name}</li>
+          ))}
+        </ul>
+      </div> */}
     </section>
   );
 }
