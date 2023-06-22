24c0e91623de1fcd78db329d0eae27cd175bc3aa
mrcbq <marcobaquero@gmail.com>
2023-06-22 10:53:38 -0500

    Add Missions test

---
+++ src/components/Missions.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import Missions from './Missions';
+import '@testing-library/jest-dom';
+
+import Mission from './Mission';
+
+const mockStore = configureStore([]);
+describe('Missions', () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      missions: {
+        missions: [
+          {
+            mission_id: '1',
+            mission_name: 'Mission 1',
+            description: 'Description 1',
+            isMember: true,
+          },
+          {
+            mission_id: '2',
+            mission_name: 'Mission 2',
+            description: 'Description 2',
+            isMember: false,
+          },
+        ],
+      },
+    });
+  });
+
+  test('renders mission list correctly', () => {
+    render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+
+    expect(screen.getByText('Mission 1')).toBeInTheDocument();
+    expect(screen.getByText('Mission 2')).toBeInTheDocument();
+
+    expect(screen.getByText('Description 1')).toBeInTheDocument();
+    expect(screen.getByText('Description 2')).toBeInTheDocument();
+
+    expect(screen.getByText('Active Member')).toBeInTheDocument();
+    expect(screen.getByText('NOT A MEMBER')).toBeInTheDocument();
+  });
+});
